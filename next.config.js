91aea667a7660e97e5c2f111405ab25b5aa2eb46
blebar25 <56796185+blebar25-lab@users.noreply.github.com>
2025-04-03 17:39:37 +0300

    Update Next.js and build configuration

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  // Désactiver la génération statique pour les routes d'API
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ];
+  experimental: {
+    serverActions: true,
   },
 }
 
